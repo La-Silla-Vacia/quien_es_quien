b2bdb4f5ecb2ef15d7ed8e15fe9537f308a3f4f1
Wietse Neven <info@wn-multimedia.nl>
2017-06-14 15:03:30 -0500

    Bit of refactoring

--- src/Components/ConnectionWires/ConnectionWires.js
+++ src/Components/ConnectionWires/ConnectionWires.js
@@ -5,8 +5,10 @@ class ConnectionWires extends Component {
   getWires() {
     const { root, connections } = this.props;
     if (!root) return;
-    const containerTop = root.top;
-    const containerLeft = root.left;
+    const containerOffset = {
+      top: root.top,
+      left: root.left
+    };
 
     return Object.keys(connections).map((key) => {
       const { source, targets, color } = connections[key];
@@ -14,33 +16,37 @@ class ConnectionWires extends Component {
       const sourceBB = source.getBoundingClientRect();
       const halfSourceSize = source.offsetWidth / 2;
       const sourceCoordinates = {
-        x: (sourceBB.left + halfSourceSize) - containerLeft,
-        y: (sourceBB.top + halfSourceSize) - containerTop
+        x: (sourceBB.left + halfSourceSize) - containerOffset.left,
+        y: (sourceBB.top + halfSourceSize) - containerOffset.top
       };
-      return targets.map((target) => {
-        if (!target || !target.parentNode) return;
-        const targetBB = target.getBoundingClientRect();
-        const halfTargetSize = target.offsetWidth / 2;
-        const targetCoordinates = {
-          x: (targetBB.left + halfTargetSize) - containerLeft,
-          y: (targetBB.top + halfTargetSize) - containerTop
-        };
+      return this.mapTargets(targets, sourceCoordinates, containerOffset, color)
+    });
+  }
 
-        const a = targetCoordinates.x - sourceCoordinates.x;
-        const b = targetCoordinates.y - sourceCoordinates.y;
+  mapTargets(targets, sourceCoordinates, containerOffset, color) {
+    return targets.map((target) => {
+      if (!target || !target.parentNode) return;
+      const targetBB = target.getBoundingClientRect();
+      const halfTargetSize = target.offsetWidth / 2;
+      const targetCoordinates = {
+        x: (targetBB.left + halfTargetSize) - containerOffset.left,
+        y: (targetBB.top + halfTargetSize) - containerOffset.top
+      };
 
-        const length = Math.sqrt( a*a + b*b );
-        const angleDeg = Math.atan2(targetCoordinates.y - sourceCoordinates.y, targetCoordinates.x - sourceCoordinates.x) * 180 / Math.PI;
+      const a = targetCoordinates.x - sourceCoordinates.x;
+      const b = targetCoordinates.y - sourceCoordinates.y;
 
-        if (targetCoordinates.x < 0 || targetCoordinates.y < 0) return;
-        return (
-          <rect key={target.id} width={length} height={1}
-                style={{transform: `rotate(${angleDeg}deg)` }}
-                x={sourceCoordinates.x} y={sourceCoordinates.y}
-                fill={color} className={s.line}
-          />
-        )
-      });
+      const length = Math.sqrt( a*a + b*b );
+      const angleDeg = Math.atan2(targetCoordinates.y - sourceCoordinates.y, targetCoordinates.x - sourceCoordinates.x) * 180 / Math.PI;
+
+      if (targetCoordinates.x < 0 || targetCoordinates.y < 0) return;
+      return (
+        <rect key={target.id} width={length} height={1}
+              style={{transform: `rotate(${angleDeg}deg)` }}
+              x={sourceCoordinates.x} y={sourceCoordinates.y}
+              fill={color} className={s.line}
+        />
+      )
     });
   }
 
