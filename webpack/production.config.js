1b429a90a084fa49196953c33a6c050c8f2f2124
Wietse Neven <info@wn-multimedia.nl>
2017-06-29 17:24:37 -0500

    Fixed a bug where the development build of react was used in production

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -43,6 +43,11 @@ const config = {
     ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
       debug: false
