f0251ed6d7e5a42cc141067645d4aa1a9fba5942
Wietse Neven <hello@wietseneven.com>
2020-07-28 14:05:44 +0200

    Better allignment of connection wires

--- src/Components/ConnectionWires/ConnectionWires.js
+++ src/Components/ConnectionWires/ConnectionWires.js
@@ -26,13 +26,13 @@ class ConnectionWires extends Component {
   }
 
   handleMouseEnter = (i, id) => {
-    this.wires[i].setAttribute('height', 5);
+    this.wires[i].setAttribute('stroke-width', 5);
     const el = document.getElementById(id);
     if (el) el.setAttribute('data-open', true);
   };
 
   handleMouseLeave = (i, id) => {
-    this.wires[i].setAttribute('height', 2);
+    this.wires[i].setAttribute('stroke-width', 2);
     const el = document.getElementById(id);
     if (el) el.removeAttribute('data-open');
   };
@@ -40,7 +40,6 @@ class ConnectionWires extends Component {
   mapTargets(targets, sourceCoordinates, containerOffset, color) {
     return targets.map((targetObj, i) => {
       const target = targetObj.el;
-      const description = targetObj.relationDescription;
       if (!target || !target.parentNode) return;
       const targetBB = target.getBoundingClientRect();
       const halfTargetSize = target.offsetWidth / 2;
@@ -49,24 +48,14 @@ class ConnectionWires extends Component {
         y: (targetBB.top + halfTargetSize) - containerOffset.top
       };
 
-      const a = targetCoordinates.x - sourceCoordinates.x;
-      const b = targetCoordinates.y - sourceCoordinates.y;
-
-      const length = Math.sqrt( a*a + b*b );
-      const angleDeg = Math.atan2(targetCoordinates.y - sourceCoordinates.y, targetCoordinates.x - sourceCoordinates.x) * 180 / Math.PI;
-
       if (targetCoordinates.x < 0 || targetCoordinates.y < 0) return;
-      return (
-        <rect key={target.id} width={length} height={2}
-              style={{transform: `rotate(${angleDeg}deg)` }}
-              x={sourceCoordinates.x} y={sourceCoordinates.y}
-              ref={(ref) => {this.wires[`${target.id}-${i}`] = ref}}
-              onMouseEnter={this.handleMouseEnter.bind(false, `${target.id}-${i}`, target.id)}
-              onMouseLeave={this.handleMouseLeave.bind(false, `${target.id}-${i}`, target.id)}
-              title={description}
-              fill={color} className={s.line}
-        />
-      )
+      return <line key={target.id} height={2}
+                   strokeWidth={2} stroke={color}
+                   x1={sourceCoordinates.x} y1={sourceCoordinates.y} x2={targetCoordinates.x} y2={targetCoordinates.y}
+                   ref={(ref) => {this.wires[`${target.id}-${i}`] = ref}}
+                   onMouseEnter={this.handleMouseEnter.bind(false, `${target.id}-${i}`, target.id)}
+                   onMouseLeave={this.handleMouseLeave.bind(false, `${target.id}-${i}`, target.id)}
+                   fill={color} className={s.line} />
     });
   }
 
